f5643f9e35de72ee538bdb2e3ae8ffe3ed801788
Source-Pallavi <59175852+Source-Pallavi@users.noreply.github.com>
2020-06-30 10:57:13 +0530

    Add project files.

---
+++ MoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleConsoleApp.MoodAnalyser
+{
+    public class MoodAnalyser
+    {
+        public Boolean AnalysisMood()
+        {
+            return true;
+        }
+    }
+}
